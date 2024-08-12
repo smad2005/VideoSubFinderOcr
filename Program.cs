7b6b0fabe825ba3d423be14abb44068f9437932a
Maxim Sokolovsky <smad2005@users.noreply.github.com>
2024-08-13 02:40:15 +0300

    Created project

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Tesseract;
+
+namespace Ocr
+{
+    class Program
+    {
+        const string TessDataPath = @"OcrData";
+        const string TxtImagesFolder = @"TXTImages";
+        const string TxtResultFolder = @"TXTResults";
+        
+        static void Main(string[] args)
+        {
+            string lang = "rus";
+            if (args is ["--help" or "/?"])
+            {
+                Console.WriteLine(
+                    """
+                    OCR for VideoSubFinder
+                    
+                    --help          Help
+                    --lang rus+eng      Choose lang: [rus, eng, rus+eng]
+                                    By default: rus
+                                    More languages: https://github.com/tesseract-ocr/tessdata
+                                    Stored in OcrData folder
+                    """);
+                return;
+            }
+
+            switch (args)
+            {
+                case ["--lang", { } selectedLang]:
+                    lang = selectedLang;
+                    break;
+            }
+
+            if (!Directory.Exists(TxtImagesFolder))
+            {
+                return;
+            }
+
+            if (!Directory.Exists(TxtResultFolder))
+            {
+                Directory.CreateDirectory(TxtResultFolder);
+            }
+            var files = Directory.GetFiles(TxtImagesFolder);
+            
+            using var engine = new TesseractEngine(TessDataPath, lang, EngineMode.Default);
+            Array.ForEach(files, filepath => HandleImage(engine, filepath));
+
+            Console.WriteLine("Done");
+        }
+
+        static void HandleImage(TesseractEngine engine, string imagePath)
+        {
+            using var img = Pix.LoadFromFile(imagePath);
+            using var page = engine.Process(img);
+            var text = page.GetText().Trim();
+            var filename = Path.GetFileNameWithoutExtension(imagePath) + ".txt";
+            
+            File.WriteAllText(Path.Combine(TxtResultFolder, filename), text);
+        }
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,28 @@
+App that help to OCR TXTImages folder
+
+# How to use
+
+Requirement: [Net Runtime 8](https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-8.0.7-windows-x64-installer)
+
+- Copy to VideoSubFinder folder
+- Run Ocr.exe
+
+# FAQ
+
+```
+OCR for VideoSubFinder
+  
+  --help          Help
+  --lang rus+eng  Choose lang: [rus, eng, rus+eng]
+                  By default: rus
+                  More languages: https://github.com/tesseract-ocr/tessdata
+                  Stored in OcrData folder
+```
+
+# How to build
+
+Requirement: NET SDK 8.0
+
+```
+dotnet publish
+```
